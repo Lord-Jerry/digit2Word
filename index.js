771f642b7a67dba235a84236a1c5295f7ea88765
Lord Jerry <lovinjerry004@gmail.com>
2019-09-13 15:51:47 -0700

    make program read number from command line

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 // TODO: make program read figures from command line
 
 const Converter = require('./converter');
+const process = require('process');
+process.stdin.setEncoding('utf8');
 
-const test = new Converter();
-console.log(test.run('9999999'));
+console.log('Please Enter Amount');
+
+process.stdin.on('readable', () => {
+  let amount = process.stdin.read() || '';
+  
+  if (! isNaN(Number(amount))) {
+    const test = new Converter();
+    console.log(test.run(amount.trim()), '\n');
+  } else {
+    console.error('Please Enter A Valid Number \n');
+    
+  }
+
+  console.log('Please Enter Amount');
+
+});
+
